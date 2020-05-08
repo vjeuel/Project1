e5e37fef87359fdf8e61011ccc6920b8aa233d6a
vjeuel <vjeuel@gmail.com>
2020-05-07 19:02:50 -0500

    starting drinks.js

---
+++ javascript/drink.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+   var drink = 
+
+   
+   $.ajax({
+      url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin" + drink,
+      method: "GET"
+   }).then(function (response) {
+
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+});
