ad6dfc2675f2d8ec7e83ae028f8b62236ad71c96
vjeuel <vjeuel@gmail.com>
2020-05-07 22:03:44 -0500

    2 arrays looping thru drinks

--- javascript/drink.js
+++ javascript/drink.js
@@ -15,6 +15,8 @@ $(document).ready(function () {
             var drinkBlock = $("<div id='drink-block'>")
             $("#user-drink").append(drinkBlock);
 
+
+
             var ingredArr = [drinksResponse.drinks[i].strIngredient1, drinksResponse.drinks[i].strIngredient2,
                            drinksResponse.drinks[i].strIngredient3, drinksResponse.drinks[i].strIngredient4,
                            drinksResponse.drinks[i].strIngredient5, drinksResponse.drinks[i].strIngredient6,
@@ -22,12 +24,34 @@ $(document).ready(function () {
                            drinksResponse.drinks[i].strIngredient9, drinksResponse.drinks[i].strIngredient10,
                            drinksResponse.drinks[i].strIngredient11, drinksResponse.drinks[i].strIngredient12,
                            drinksResponse.drinks[i].strIngredient13, drinksResponse.drinks[i].strIngredient4,
-                           drinksResponse.drinks[i].strIngredient15];
+               drinksResponse.drinks[i].strIngredient15].filter(function(badIngred) {
+                  
+                  return badIngred !== null;
+               });
+            
+            var measurArr = [drinksResponse.drinks[i].strMeasure1, drinksResponse.drinks[i].strMeasure2,
+                           drinksResponse.drinks[i].strMeasure3, drinksResponse.drinks[i].strMeasure4,
+                           drinksResponse.drinks[i].strMeasure5, drinksResponse.drinks[i].strMeasure6,
+                           drinksResponse.drinks[i].strMeasure7, drinksResponse.drinks[i].strMeasure8,
+                           drinksResponse.drinks[i].strMeasure9, drinksResponse.drinks[i].strMeasure10,
+                           drinksResponse.drinks[i].strMeasure11, drinksResponse.drinks[i].strMeasure12,
+                           drinksResponse.drinks[i].strMeasure13, drinksResponse.drinks[i].strMeasure4,
+                           drinksResponse.drinks[i].strMeasure15].filter(function (badMeasur) {
+                  return badMeasur !== null;
+            });
+                     
+            
+            var ingredArrLoop = "";
+            for (let i = 0; i < ingredArr.length; i++) {
+               ingredArrLoop += "<p>ingredArr[i]" + ": " + "measurArr[i]</p>";
+               
+            }
+            
          
 
             var drinkName = $("<h3 class='drink-name'>").text(drinksResponse.drinks[i].strDrink);
             var drinkPic = $("<img class='drink-pic'>").attr("src", drinksResponse.drinks[i].strDrinkThumb);
-            var drinkIngred = $("<p class='drink-ingred'>").text("Ingredients: " + " " + ingredArr);
+            var drinkIngred = $("<p class='drink-ingred'>").text("Ingredients: " + " " + ingredArrLoop);
             var drinkInstr = $("<p class='drink-instr'>").text(drinksResponse.drinks[i].strInstructions);
             
             drinkBlock.append(drinkName, drinkPic, drinkIngred, drinkInstr);
